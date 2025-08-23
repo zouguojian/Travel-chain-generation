7ee3a6f3dd3da46acb86fc660f007549cba043b5
zouguojian <873374677@qq.com>
2025-08-23 10:42:28 +0800

    data/Dataload.py

--- data/Dataload.py
+++ data/Dataload.py
@@ -26,45 +26,6 @@ class DeepFMDataset(Dataset):
         """获取指定索引的样本，返回数据和标签对"""
         return self.data_list[idx], self.labels_list[idx]
 
-
-# 自定义collate_fn，用于动态填充变长序列以形成批次
-def collate_fn(batch):
-    """
-    将变长序列样本填充到统一长度，形成批次张量。
-
-    参数:
-        batch: list of (data, label) tuples, where data is [L, D] and label is [L, 1].
-
-    返回:
-        padded_x: [B, max_l, D], 填充后的输入数据张量，填充值为0。
-        padded_y: [B, max_l, 1], 填充后的标签张量，填充值为-1（用于掩码）。
-    """
-    x, y = zip(*batch)  # 分离数据和标签
-    lengths = [len(xi) for xi in x]  # 获取每个样本的序列长度
-    max_l = max(lengths)  # 批次中的最大序列长度
-    # 初始化填充张量，数据用0填充，标签用-1填充以标记无效位置
-    padded_x = torch.zeros(len(batch), max_l, x[0].shape[1])
-    padded_y = torch.ones(len(batch), max_l, 1) * -1  # -1表示填充位置
-    # 将每个样本填充到最大长度
-    for i in range(len(batch)):
-        padded_x[i, :lengths[i]] = x[i]
-        padded_y[i, :lengths[i]] = y[i]
-    return padded_x, padded_y
-
-
-
-
-def collate_fn(batch):
-    # Pad sequences to the longest in the batch
-    # Input: batch (list of (sequence, label, seq_length))
-    # Output: padded_data [B, max_len, D], labels [B], seq_lengths (list)
-    data, labels, seq_lengths = zip(*batch)
-    max_len = max(seq_lengths)
-    padded_data = torch.zeros(len(data), max_len, data[0].size(-1))
-    for i, seq in enumerate(data):
-        padded_data[i, :len(seq)] = seq
-    return padded_data, torch.tensor(labels), seq_lengths
-
 class StandardScaler:
     """
     Standard the input
@@ -81,57 +42,114 @@ class StandardScaler:
         return (data * self.std) + self.mean
 
 class VariableLengthDataset(Dataset):
-    def __init__(self, num_samples, d_model, min_len=5, max_len=15):
+    def __init__(self, x, y, l, mean, std):
+        # x 表示模型的输入数据 [B, D]
+        # y 表示标签 ([B, L], [B, 1])
+        # l 表示每个路径的长度 [B, 1]
         # Initialize dataset with variable-length sequences
         self.data = []
         self.labels = []
         self.seq_lengths = []
-        for _ in range(num_samples):
-            # Random sequence length between min_len and max_len
-            seq_len = np.random.randint(min_len, max_len + 1)
-            seq = torch.randn(seq_len, d_model)
-            # Binary label based on sum parity
-            label = (seq.sum(dim=0).sum() % 2).long()
-            self.data.append(seq)
-            self.labels.append(label)
-            self.seq_lengths.append(seq_len)
+        for i in range(l.shape[0]):
+            # variable sequence length for x and y
+            x_1 = np.array(np.tile(x[i][:-1], (l[i], 1)), dtype=np.float32)
+            x_2 = (np.array(np.reshape(x[i][-1], [l[i], 1]), dtype=np.float32) - mean) / std # 归一化
+            self.data.append(torch.FloatTensor(np.concatenate((x_1, x_2), axis=1)))
+            self.labels.append(torch.FloatTensor(np.array([y[i][1]] + y[i][0], dtype=np.float32)))
+            self.seq_lengths.append(l[i])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         # Return sequence, label, and sequence length
+        # x: [0, 0, 13, 0, 1431, 1, 14388.9, 14828.39], y: [25.567, 11.9, 13.667], l: [2]
         return self.data[idx], self.labels[idx], self.seq_lengths[idx]
+
+
+# 自定义collate_fn，用于动态填充变长序列以形成批次
+def collate_fn(batch):
+    """
+    将变长序列样本填充到统一长度，形成批次张量。
+
+    参数:
+        batch: list of (data, label) tuples, where data is [L, D] and label is [L, 1].
+
+    返回:
+        padded_x: [B, max_l, D], 填充后的输入数据张量，填充值为0。
+        padded_y: [B, max_l, 1], 填充后的标签张量，填充值为-1（用于掩码）。值得注意，第一个维度是总时间，后面是路段时间
+    """
+    # Pad sequences to the longest in the batch
+    # Input: batch (list of (sequence, label, seq_length))
+    # Output: padded_data [B, max_len, D], labels [B], seq_lengths (list)
+
+    data, labels, seq_lengths = zip(*batch)  # 分离数据: [B, 6 + L]、标签: [B, 1 + L]、长度: [B]
+    max_l = max(seq_lengths)  # 批次中的最大序列长度
+    # 初始化填充张量，数据用0填充，标签用-1填充以标记无效位置
+    padded_x = torch.zeros(len(batch), max_l, data[0].shape[1]) # [B, max_l, 6 + 1]
+    padded_y = torch.ones(len(batch), max_l + 1) * -1  # -1表示填充位置 [B, 1 + max_l]
+    # 将每个样本填充到最大长度
+    for i in range(len(batch)):
+        padded_x[i, :seq_lengths[i]] = data[i]
+        padded_y[i, :seq_lengths[i] + 1] = labels[i]
+    return padded_x, padded_y, seq_lengths
 
 def load_dataset(dataset_dir, batch_size, test_batch_size=None, **kwargs):
     data = {}
+    max_dow, max_mod= 7, 1440
     # 加载数据到data字典中
     for category in ['train', 'val', 'test']:
         cat_data = np.load(os.path.join(dataset_dir, category + '.npz'), allow_pickle=True)
         data['x_' + category] = cat_data['x']
         data['y_' + category] = cat_data['y']
         data['l_' + category] = cat_data['l']
+        xdis_mean, xdis_std = cat_data['xdis_mean'], cat_data['xdis_std']
+        ytra_mean, ytra_std = cat_data['ytra_mean'], cat_data['ytra_std']
+        ytol_mean, ytol_std = cat_data['ytol_mean'], cat_data['ytol_std']
         del cat_data
 
-    train_dataset = VariableLengthDataset(data['x_train'], data['y_train'], data['l_train'])
-    val_dataset = VariableLengthDataset(data['x_val'], data['y_val'], data['l_val'])
-    test_dataset = VariableLengthDataset(data['x_test'], data['y_test'], data['l_test'])
+    total_x = np.concatenate((data['x_' + 'train'], data['x_' + 'val'], data['x_' + 'test']))
+    max_city, max_plate, max_v_type, max_rote = np.max(total_x[:, 0]) + 1, np.max(total_x[:, 1]) + 1, np.max(total_x[:, 2]) + 1, np.max(total_x[:, 5]) + 1
+    del total_x
 
-    scaler = StandardScaler(mean=data['y_train'][..., 0].mean(), std=data['y_train'][..., 0].std())
-    # Data format
-    for category in ['train', 'val', 'test']:
-        data['x_' + category][..., 0] = scaler.transform(data['x_' + category][..., 0])
-        data['y_' + category][..., 0] = scaler.transform(data['y_' + category][..., 0])
+    train_dataset = VariableLengthDataset(data['x_train'], data['y_train'], data['l_train'], xdis_mean, xdis_std)
+    val_dataset = VariableLengthDataset(data['x_val'], data['y_val'], data['l_val'], xdis_mean, xdis_std)
+    test_dataset = VariableLengthDataset(data['x_test'], data['y_test'], data['l_test'], xdis_mean, xdis_std)
 
     # Generate synthetic variable-length dataset
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
     test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False, collate_fn=collate_fn)
 
-    return train_loader, val_loader, test_loader, scaler
+    return train_loader, val_loader, test_loader, max_city, max_plate, max_v_type, max_dow, max_mod, max_rote, ytra_mean, ytra_std, ytol_mean, ytol_std
 
 
-cat_data = np.load('train' + '.npz', allow_pickle=True)
-for key in cat_data.keys():
-    print(key)
-print(cat_data['x'][:,-1].mean())
+# train_loader, val_loader, test_loader, max_city, max_plate, max_v_type, max_dow, max_mod, max_rote, ytra_mean, ytra_std, ytol_mean, ytol_std = load_dataset('/Users/zouguojian/Travel-chain-generation/data', 32, test_batch_size=1)
+# for batch_x, batch_y, batch_l in train_loader:
+#     print(batch_x.shape)
+#     print(max_city, max_plate, max_v_type, max_dow, max_mod, max_rote, ytra_mean, ytra_std, ytol_mean, ytol_std)
+
+'''
+data = {}
+for category in ['train', 'val', 'test']:
+    cat_data = np.load(os.path.join(category + '.npz'), allow_pickle=True)
+    data['x_' + category] = cat_data['x']
+    data['y_' + category] = cat_data['y']
+    data['l_' + category] = cat_data['l']
+    del cat_data
+
+total_x = np.concatenate((data['x_' + 'train'], data['x_' + 'val'], data['x_' + 'test']))
+print(total_x[:, -1])
+x_mean, x_std = np.mean(np.reshape(total_x[:, -1], [-1])), np.std(np.reshape(total_x[:, -1], [-1]))
+print(x_mean, x_std)
+
+print(data['x_train'][-1], data['y_' + category][-1], data['l_' + category][-1])
+train_dataset = VariableLengthDataset(data['x_train'], data['y_train'], data['l_train'])
+print(train_dataset.__getitem__(-1))
+'''
+
+
+# train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
+
+# for batch_x, batch_y, batch_l in train_loader:
+#     print(batch_l)
