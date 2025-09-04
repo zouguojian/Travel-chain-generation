4391703b0d1a460a75dfc2b48bd9590e132f4867
zouguojian <873374677@qq.com>
2025-09-04 17:20:19 +0800

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import torch
-import torch.nn as nn
 import numpy as np
-from sympy import print_rcode
-
-from data.Dataload import load_dataset
+from data.Dataload import load_dataset, load_adjs
 from model.DMTLN import DmtlnModel
 import argparse
 from utils.utils_ import log_string,count_parameters, _compute_loss
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_his', type=int, default=12, help='history steps')
@@ -19,21 +17,22 @@ parser.add_argument('--decay_epoch', type=int, default=10, help='decay epoch')
 
 parser.add_argument('--num_features', type=int, default=7, help='total number of features')
 parser.add_argument('--emb_size', type=int, default=32, help='size of embedding')
-parser.add_argument('--class_num', type=int, default=11, help='number of classes')
+parser.add_argument('--class_num', type=int, default=10, help='number of classes')
 parser.add_argument('--batch_size', type=int, default=32, help='batch size')
 parser.add_argument('--num_epochs', type=int, default=10, help='number of epochs')
 parser.add_argument('--lr', type=float, default=0.001, help='initial learning rate')
 parser.add_argument('--max_len', type=int, default=10, help='weight decay')
 parser.add_argument('--traffic_file', default='./data/', help='traffic file')
 parser.add_argument('--model_file', default='./data/best_model.pth', help='save the model to disk')
+parser.add_argument('--segment_station_with_distance', default='./data/states/segment_station_with_distance.csv', help='segment station with distance')
 parser.add_argument('--log_file', default='./data/log', help='log file')
 args = parser.parse_args()
 
 log = open(args.log_file, 'w')
 log_string(log, str(args)[10: -1])
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 log_string(log, 'device using: {:}'.format(device))
 
 # 数据集加载
@@ -42,6 +41,9 @@ train_loader, val_loader, test_loader, max_city, max_plate, max_v_type, max_dow,
 log_string(log, f'max_city:   {max_city}\t\tmax_plate:   {max_plate}\t\tmax_v_type:   {max_v_type}\t\tmax_rote:   {max_route}')
 log_string(log, f'ytra_mean:   {ytra_mean:.4f}\t\tytra_std:   {ytra_std:.4f}')
 log_string(log, f'ytol_mean:   {ytol_mean:.4f}\t\tytol_std:   {ytol_std:.4f}')
+geo_flow_adj, sem_flow_adj = load_adjs(geo_adj_fil = 'data/states/geo_flow_adj.npz', sem_adj_fil = 'data/states/sem_flow_adj.npz', device=device)
+geo_speed_adj, sem_speed_adj = load_adjs(geo_adj_fil = 'data/states/geo_speed_adj.npz', sem_adj_fil = 'data/states/sem_speed_adj.npz', device=device)
+df = pd.read_csv(args.segment_station_with_distance, encoding='utf-8')
 log_string(log, 'data loaded!')
 
 # 训练函数
@@ -54,10 +56,10 @@ def train_model(model, train_loader, val_loader, epochs, lr, device, log):
         model.train()
         train_loss = 0
         iteration = 1
-        for batch_x, batch_y, batch_l in train_loader:
-            batch_x, batch_y = (batch_x.to(device), batch_y.to(device))
+        for batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_y, batch_cla, batch_l in train_loader:
+            batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_y, batch_cla = (batch_x.to(device), batch_flow.to(device), batch_speed.to(device), batch_dow.to(device), batch_mod.to(device), batch_y.to(device), batch_cla.to(device))
             optimizer.zero_grad()
-            pred1, pred2, pred3 = model(batch_x, batch_l)
+            pred1, pred2, pred3 = model(batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_cla, batch_l, df, adjs = [geo_flow_adj, sem_flow_adj, geo_speed_adj, sem_speed_adj])
 
             # 将标签中的-1替换为0以满足loss的要求
             valid_batch_y = torch.where(batch_y[:,1:] == -1, torch.zeros_like(batch_y[:,1:]), batch_y[:,1:])  # [B, current_max_L, 1]
@@ -89,9 +91,9 @@ def validate_model(model, val_loader, device):
     model.eval()
     val_loss = 0
     with torch.no_grad():
-        for batch_x, batch_y, batch_l in val_loader:
-            batch_x, batch_y = (batch_x.to(device), batch_y.to(device))
-            pred1, pred2, pred3 = model(batch_x, batch_l) # pred1是路段时间，pred2是路线时间
+        for batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_y, batch_cla, batch_l in val_loader:
+            batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_y, batch_cla = (batch_x.to(device), batch_flow.to(device), batch_speed.to(device), batch_dow.to(device), batch_mod.to(device), batch_y.to(device), batch_cla.to(device))
+            pred1, pred2, pred3 = model(batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_cla, batch_l, df, adjs = [geo_flow_adj, sem_flow_adj, geo_speed_adj, sem_speed_adj]) # pred1是路段时间，pred2是路线时间
 
             # 计算每个任务的损失
             # 将标签中的-1替换为0以满足loss的要求
@@ -114,9 +116,9 @@ def test_model(model, test_loader, device):
     labels_dict = {i:[] for i in range(max_route)}
     preds_dict = {i:[] for i in range(max_route)}
     with torch.no_grad():
-        for batch_x, batch_y, batch_l in test_loader:
-            batch_x, batch_y = (batch_x.to(device), batch_y.to(device))
-            pred1, pred2, pred3 = model(batch_x, batch_l)
+        for batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_y, batch_cla, batch_l in test_loader:
+            batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_y, batch_cla = (batch_x.to(device), batch_flow.to(device), batch_speed.to(device), batch_dow.to(device), batch_mod.to(device), batch_y.to(device), batch_cla.to(device))
+            pred1, pred2, pred3 = model(batch_x, batch_flow, batch_speed, batch_dow, batch_mod, batch_cla, batch_l, df, adjs = [geo_flow_adj, sem_flow_adj, geo_speed_adj, sem_speed_adj])
             preds_dict[batch_x.cpu().numpy()[0, 0, 5]].append(np.concatenate((pred2.cpu().numpy(), pred1.cpu().numpy()), axis=1))
             labels_dict[batch_x.cpu().numpy()[0, 0, 5]].append(batch_y.cpu().numpy())
 
@@ -133,7 +135,6 @@ def test_model(model, test_loader, device):
         labels_dict[i] = np.array(labels_dict[i], dtype=np.float32)
         np.savez_compressed('data/results/DMTLN-' + str(i) + '-YINCHUAN', **{'prediction': preds_dict[i], 'truth': labels_dict[i]})
     print(f'Test1 Loss: {test1_loss / len(test_loader):.4f}, Task 2 Loss: {test2_loss / len(test_loader):.4f}')
-
 
 # 主函数
 def main():
@@ -153,10 +154,10 @@ def main():
     log_string(log, 'trainable parameters: {:,}'.format(parameters))
 
     # 训练和验证
-    # train_model(model, val_loader, val_loader, args.num_epochs, args.lr, device, log)
+    train_model(model, val_loader, val_loader, args.num_epochs, args.lr, device, log)
 
     # 测试
-    test_model(model, test_loader, device)
+    # test_model(model, test_loader, device)
 
 
 if __name__ == "__main__":
